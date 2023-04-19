96a536473eb27850de334f3bc7e27e7d85a02149
Cliff Likovo <clifflikovodeveloper@gmail.com>
2023-04-19 23:32:36 +0200

    update Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -4,9 +4,10 @@ interface IButton {
   children: React.ReactNode
 }
 
+// TODO make different variants of buttons.
 function Button(props: IButton): JSX.Element {
   return (
-    <button className="flex flex-row justify-between items-center space-x-6 px-6 py-3 hover:bg-action text-heading text-lg font-heading font-semibold tracking-wide w-fit border-action border-2">
+    <button className="flex flex-row items-center justify-between space-x-2 px-6 py-3 hover:bg-action text-heading text-lg font-heading font-semibold tracking-wide w-fit border-action border-2">
       {props.children}
     </button>
   )
