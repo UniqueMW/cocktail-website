8bbc86620ad82beb7c9984d5de5b9a84453029cc
Cliff Likovo <clifflikovodeveloper@gmail.com>
2023-05-04 21:31:02 +0200

    update debounce

--- utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-function debounce(callback: () => void, duration: number): () => void {
-  let timeOutId: string | number | NodeJS.Timeout | undefined
-  return (): void => {
-    clearTimeout(timeOutId)
-    timeOutId = setTimeout(callback, duration)
-  }
-}
-
-export default debounce
--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 import randomize from './randomize/randomize'
 import fetcher from './fetcher/fetcher'
 import groupValues from './groupValues/groupValues'
-import debounce from './debounce'
+import debounce from './debounce/debounce'
 import addAndRemove, {
   checkDrinkInBookmark
 } from './BookmarkInterface/BookmarkInterface'
